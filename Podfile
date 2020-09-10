4c1e7bb1981df220e40c2654ee6aa8e4ae3bdf30
JeongSuhong <ghjkdfe@gmail.com>
2020-09-10 19:37:31 +0900

    pod Kingfisher 추가 & UI 수정중

--- Podfile
+++ Podfile
@@ -5,6 +5,8 @@ target 'YoutubePlayer' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
+  pod 'Kingfisher', '~> 5.15'
+
   # Pods for YoutubePlayer
     
 
--- Pods/Target Support Files/Pods-YoutubePlayer/Pods-YoutubePlayer-frameworks.sh
+++ Pods/Target Support Files/Pods-YoutubePlayer/Pods-YoutubePlayer-frameworks.sh
@@ -197,14 +197,10 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/GoogleAPIClientForREST/GoogleAPIClientForREST.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/GoogleAPIClientForREST/GoogleAPIClientForREST.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
